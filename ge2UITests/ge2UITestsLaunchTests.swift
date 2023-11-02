5b2bdd3c14091dc830dedd8934ddc55087da3cfb
Adam Socki <socki.adam@gmail.com>
2023-11-02 13:09:37 -0500

    init

---
+++ ge2UITests/ge2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ge2UITestsLaunchTests.swift
+//  ge2UITests
+//
+//  Created by socki on 11/1/23.
+//
+
+import XCTest
+
+final class ge2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
